6def3bb75fe9c40d44ee95531a0c389c4821f897
Zahid <55988827+Zarhad-01@users.noreply.github.com>
2020-02-20 12:08:55 +0000

    Helper class written, need to comment

---
+++ public/js/controller/Help.js
@@ -0,0 +1,32 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const Help = {
+    hashPW (pw) {
+        salt = bcrypt.genSaltSync(10);
+        return bcrypt.hashSync(pw, salt);
+    },
+
+
+    passwordCompare (hashedPW, pw) {
+        return bcrypt.compare(pw, hashedPW)
+    },
+
+    
+    validateEmail (email) {
+        return /\S+@\S+\.\S+/.test(email);
+    },
+
+    generateWebToken(userID) {
+        const webToken = jwt.sign(
+        {userId: userID},
+        process.env.SECRET,
+        {expiresIn: "14d"}
+        );
+    }
+
+
+
+
+
+}
