3353b4769958f5a4243cc8914ced479da6336ca7
Lyxnx <a.julnes@gmail.com>
2020-02-21 15:23:55 +0000

    add default value for timestamps

--- init.sql
+++ init.sql
@@ -16,7 +16,7 @@ CREATE TABLE chat_message (
 						message_id int PRIMARY KEY,
 						sender_id int REFERENCES user_account(user_id) NOT NULL,
 						receiver_id int REFERENCES user_account(user_id) NOT NULL,
-						message_time timestamp NOT NULL,
+						message_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 						message_content varchar(255) NOT NULL);
 
 CREATE TABLE report (
@@ -29,11 +29,11 @@ CREATE TABLE live_session (
 							live_session_id int PRIMARY KEY,
 							live_sesion_name varchar(50) NOT NULL,
 							live_session_owner_id int REFERENCES user_account(user_id) NOT NULL,
-							live_session_date timestamp NOT NULL);
+							live_session_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP);
 
 CREATE TABLE live_message (
 							live_message_id int PRIMARY KEY,
 							live_session_id int REFERENCES live_session(live_session_id) NOT NULL,
 							user_id int REFERENCES user_account(user_id) NOT NULL,
-							live_message_time timestamp NOT NULL,
+							live_message_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 							live_message_content varchar(255) NOT NULL);
