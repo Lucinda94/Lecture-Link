ad37e2faa34b97c07dcf18291866553bed741347
Lyxnx <a.julnes@gmail.com>
2020-02-21 15:36:45 +0000

    more chat stuff

--- chats/ChatController.js
+++ chats/ChatController.js
@@ -10,7 +10,16 @@ router.get('/:id', async (req, res) => {
         const chats = await db.getChatsForUser(req.params.id);
         res.status(200).send(chats);
     } catch (err) {
-        res.status(500).send('Could not fetch chats');
+        return res.status(500).send('Could not fetch chats');
+    }
+});
+
+// send a message to a user eg. POST /api/chat/RECEIVER_ID with the sender id and content in the request body
+router.post('/:id', async (req, res) => {
+    try {
+        const msg = await db.sendMessage(req.params.id, req.body.receiver, req.body.content);
+    } catch (err) {
+        return res.status(500).send('Could not send chat message');
     }
 });
 
--- postgres-db.js
+++ postgres-db.js
@@ -14,13 +14,22 @@ module.exports.getAllUsers = async () => {
 };
 
 module.exports.getUser = async (id) => {
-    const { rows } = await client.query('SELECT * FROM user_account WHERE user_id = $1', [id]);
+    const { rows } = await pool.query('SELECT * FROM user_account WHERE user_id = $1', [id]);
     return rows[0];
 };
 
 // CHATS
 
 module.exports.getChatsForUser = async (userId) => {
-    const { rows } = await client.query('SELECT * FROM chat_message WHERE sender_id = $1 OR receiver_id = $1', [userId]);
+    const { rows } = await pool.query('SELECT * FROM chat_message WHERE sender_id = $1 OR receiver_id = $1', [userId]);
     return rows;
+};
+
+module.exports.sendMessage = async (senderId, receiverId, content) => {
+    const { rows } = await pool.query(
+        'INSERT INTO chat_message (sender_id, receiver_id, message_content) VALUES ($1, $2, $3)', 
+        [senderId, receiverId, content]
+    );
+
+    return rows; // idk if it returns anything on an insert so this could be wrong
 };
