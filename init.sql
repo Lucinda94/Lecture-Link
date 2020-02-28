f0ece5c339ce660e2386fece2b7aa910fe11fa0e
JackSines <48910905+JackSines@users.noreply.github.com>
2020-02-28 15:42:49 +0000

    Updated database
    Added ability for messages to be seen and to be archived.
    Also added a 'Lecturer' type of relationship so students can have their lecturers identified

--- init.sql
+++ init.sql
@@ -7,7 +7,7 @@ CREATE TABLE user_account(
 					user_password varchar(50) NOT NULL,
 					user_status status NOT NULL);
 
-CREATE TYPE relationship as enum('Saved', 'Blocked');
+CREATE TYPE relationship as enum('Saved', 'Blocked', 'Lecturer');
 CREATE TABLE user_relationship (
 								user_id int PRIMARY KEY REFERENCES user_account(user_id),
 								type_of_relationship relationship NOT NULL);
@@ -17,6 +17,8 @@ CREATE TABLE chat_message (
 						sender_id int REFERENCES user_account(user_id) NOT NULL,
 						receiver_id int REFERENCES user_account(user_id) NOT NULL,
 						message_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+						message_seen boolean NOT NULL,
+						message_archive boolean NOT NULL,
 						message_content varchar(255) NOT NULL);
 
 CREATE TABLE report (
