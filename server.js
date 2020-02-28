4a859d7ca4e09a890222044d18b0f72eebeb1437
Austin <47179017+austcollins@users.noreply.github.com>
2020-02-28 19:52:35 +0000

    Cleaned up

--- server.js
+++ server.js
@@ -5,12 +5,29 @@ const passport = require('passport');
 const flash = require('express-flash');
 const session = require('express-session');
 
-// set up passport
+/****
+ * Set up passport
+ */
 const initPassport = require('./passport-config');
+// TODO: connect passport to the database
 initPassport(
   passport, 
-  email => { return users.find(user => user.email === email) },
-  id => { return users.find(user => user.id === id) }
+  email => {
+    // where given 'email' get 'id' and 'password' from the database.
+    const userData = {
+      id: '45',
+      password: 'password from database'
+    };
+    return users.find(user => user.email === email) // for testing will return userData
+  },
+  id => {
+    const userData = {
+      // where given 'id' get 'password' from the database.
+      id: id,
+      password: 'password from database'
+    };
+    return users.find(user => user.id === id) // for testing will return userData
+  }
   );
 
 // FOR TESTING
@@ -23,6 +40,9 @@ const users = [
 }
 ];
 
+/****
+ * Set up application
+ */
 // set ejs as our view engine
 app.set('view engine', 'ejs');
 // tell the app that we are receiving informaiton from forms
@@ -37,60 +57,60 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-// create the login get and post routes
+/****
+ * 
+ * Login/Register/Logout Routes
+ * 
+ */
+// Returns login page, only if they are not logged in
 app.get('/login', checkNotLoggedIn, (req, res) => {
 res.render('pages/login');
 })
-
+// when the login form is submitted, passport handles the login
 app.post('/login', checkNotLoggedIn, passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/login',
   failureFlash: true
 }))
-
+// return the registration page
 app.get('/register', checkNotLoggedIn, (req, res) => {
   res.render('pages/register');
-  })
-  
-  app.post('/register', checkNotLoggedIn, async (req, res, next) => {
-    try {
-      // hash the password the user has sent. Use await as this is async
-      const passHash = await bcrypt.hash(req.body.password, 10);
-      // this would be put in database
-      users.push({
-        id: Date.now().toString(), // this will be made by database
-        name: req.body.name,
-        email: req.body.email,
-        password: passHash
-      });
-      // TODO: send confirmation email
+ })
+// when the registration form is submitted
+app.post('/register', checkNotLoggedIn, async (req, res, next) => {
+  try {
+    // hash the password the user has sent. Use await as this is async
+    const passHash = await bcrypt.hash(req.body.password, 10);
+    // just for testing: push this to database
+    users.push({
+      id: Date.now().toString(), // this will be made by database
+      name: req.body.name,
+      email: req.body.email,
+      password: passHash
+    });
+    // TODO: send confirmation email
 
-      // this will redirect to confirm email page.
-      res.redirect('/login');
-    } catch (err) {
-      console.log(err);
-      // something went wrong try again.
-      res.redirect('/register');
-    }
-    console.log(users);
-  })
+    // registration worked so send to login page
+    res.redirect('/login');
 
-  app.get('/logout', (req, res) => {
-    req.logOut(); // tell passport to log the user out
-    res.redirect('/login'); // redirect to the login page
-  })
+  } catch (err) {
+    console.log(err);
+    // something went wrong, try again.
+    res.redirect('/register');
+  }
+})
+// handles logging out
+app.get('/logout', (req, res) => {
+  req.logOut(); // tell passport to log the user out
+  res.redirect('/login'); // redirect to the login page
+})
 
-
-
-// we use res.render to load up an ejs file
-// res.render will look in the view file
-
+/*****
+ * Main application page
+ */
 // main application
 app.get('/', checkLoggedIn, function(req, res) {
-    
   res.render('pages/main-application');
-  //res.redirect('/login')
-
 });
 
 // this will handle the /user/... api endpoints
@@ -100,21 +120,29 @@ app.use('/api/user', UserController);
 const ChatController = require('./chats/ChatController');
 app.use('/api/chats', ChatController);
 
-// static content
+/****
+ * Serve static content
+ */
 app.use('/static', express.static('public'));
 
-// 404 route
+/****
+ * Anything that isn't routed, show 404 page.
+ */
 app.get('*', function(req, res) {
     res.render('pages/404');
 });
 
-// checks if a person is logged in
+/****
+ * Login checks (for use when limiting routes)
+ */
+// checks if user is logged in, if not redirects to login page
 function checkLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
   res.redirect('/login');
 };
+// checks if the user is logged in, if so redirects to main applicaiton
 function checkNotLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     return res.redirect('/');
@@ -122,6 +150,9 @@ function checkNotLoggedIn(req, res, next) {
   next();
 };
 
+/****
+ * Start the server
+ */
 app.listen(8080, (err) => {
     if (err) console.log('Could not start server', err);
     console.log('Server listening on port 8080');
