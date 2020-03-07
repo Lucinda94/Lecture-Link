1246d22be4962883e695d58fb2c8835a14361417
Lucinda94 <lucindacole@hotmail.co.uk>
2020-03-07 20:18:46 +0000

    Started testing script

--- testing/main.test.js
+++ testing/main.test.js
@@ -6,3 +6,13 @@ test('adds 1 + 2 to equal 3', () => {
     expect(sum(1,2)).toBe(3);
 });
 */
+
+const appendMessageToChat = require('./main');
+
+test('Appends Msg to chat', () => {
+    expect()
+})
+
+test('Get Messages', () => {
+    expect(getMessages(a)).toBe(false);
+})
