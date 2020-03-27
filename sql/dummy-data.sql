3b7b0da01322612b03cba866f4f0c1a58b96b82d
Austin <47179017+austcollins@users.noreply.github.com>
2020-03-27 12:10:07 +0000

    working

--- sql/dummy-data.sql
+++ sql/dummy-data.sql
@@ -27,23 +27,23 @@ INSERT INTO user_account (
 							user_password,
 							user_status)
 					VALUES 
-					('44', 'Austin', 'Collins', 'up904254@myport.ac.uk', '$2b$10$8G5w9Csq7yYBEziSit8HNuDomO0fRdF4GPzAYgTPegcsWVpdrB0My', 'Online'),
-					('2', 'Zahid', 'Awan', '', '$2b$10$8G5w9Csq7yYBEziSit8HNuDomO0fRdF4GPzAYgTPegcsWVpdrB0My', 'Online'),
-					('3', 'Alistair', 'Julnes', 'up897828@myport.ac.uk', '$2b$10$8G5w9Csq7yYBEziSit8HNuDomO0fRdF4GPzAYgTPegcsWVpdrB0My', 'Online'),
-					('4', 'Jack', 'Sines', 'up901354@myport.ac.uk', '$2b$10$8G5w9Csq7yYBEziSit8HNuDomO0fRdF4GPzAYgTPegcsWVpdrB0My', 'Online'),
-					('5', 'Lucinda', 'Cole', 'up883852@myport.ac.uk', '$2b$10$8G5w9Csq7yYBEziSit8HNuDomO0fRdF4GPzAYgTPegcsWVpdrB0My', 'Online'),
-					('6', 'Lybin', 'Babu', 'up898707@myport.ac.uk', '$2b$10$8G5w9Csq7yYBEziSit8HNuDomO0fRdF4GPzAYgTPegcsWVpdrB0My', 'Online'),
+					('5', 'Austin', 'Collins', 'up904254@myport.ac.uk', '$2b$10$8G5w9Csq7yYBEziSit8HNuDomO0fRdF4GPzAYgTPegcsWVpdrB0My', 'Online'),
+					('6', 'Zahid', 'Awan', 'up895058@myport.ac.uk', '$2b$10$8G5w9Csq7yYBEziSit8HNuDomO0fRdF4GPzAYgTPegcsWVpdrB0My', 'Online'),
+					('7', 'Alistair', 'Julnes', 'up897828@myport.ac.uk', '$2b$10$8G5w9Csq7yYBEziSit8HNuDomO0fRdF4GPzAYgTPegcsWVpdrB0My', 'Online'),
+					('8', 'Jack', 'Sines', 'up901354@myport.ac.uk', '$2b$10$8G5w9Csq7yYBEziSit8HNuDomO0fRdF4GPzAYgTPegcsWVpdrB0My', 'Online'),
+					('9', 'Lucinda', 'Cole', 'up883852@myport.ac.uk', '$2b$10$8G5w9Csq7yYBEziSit8HNuDomO0fRdF4GPzAYgTPegcsWVpdrB0My', 'Online'),
+					('10', 'Lybin', 'Babu', 'up898707@myport.ac.uk', '$2b$10$8G5w9Csq7yYBEziSit8HNuDomO0fRdF4GPzAYgTPegcsWVpdrB0My', 'Online'),
 
-					('7', 'Alden', 'Cantrell', '2@myport.ac.uk', '0', 'Away'),
-					('8', 'Kierra', 'Gentry', '3@myport.ac.uk', '0', 'Busy'),
-					('9', 'Pierre', 'Cox', '4@myport.ac.uk', '0', 'Away'),
-					('10', 'Thomas', 'Crane', '5@myport.ac.uk', '0', 'Online'),
-					('11', 'Miranda', 'Shaffer', '6@myport.ac.uk', '0', 'Online'),
-					('12', 'Bradyn', 'Kramer', '7@myport.ac.uk', '0', 'Away'),
-					('13', 'Aiden', 'Cummings', '8@myport.ac.uk', '0', 'Busy'),
-					('14', 'Cassie', 'Dean', '9@myport.ac.uk', '0', 'Away'),
-					('15', 'Cailyn', 'Chapman', '10@myport.ac.uk', '0', 'Online'),
-					('16', 'Katrina', 'Roy', '11@myport.ac.uk', '0', 'Away');
+					('11', 'Alden', 'Cantrell', '2@myport.ac.uk', '0', 'Away'),
+					('12', 'Kierra', 'Gentry', '3@myport.ac.uk', '0', 'Busy'),
+					('13', 'Pierre', 'Cox', '4@myport.ac.uk', '0', 'Away'),
+					('14', 'Thomas', 'Crane', '5@myport.ac.uk', '0', 'Online'),
+					('15', 'Miranda', 'Shaffer', '6@myport.ac.uk', '0', 'Online'),
+					('16', 'Bradyn', 'Kramer', '7@myport.ac.uk', '0', 'Away'),
+					('17', 'Aiden', 'Cummings', '8@myport.ac.uk', '0', 'Busy'),
+					('18', 'Cassie', 'Dean', '9@myport.ac.uk', '0', 'Away'),
+					('19', 'Cailyn', 'Chapman', '10@myport.ac.uk', '0', 'Online'),
+					('20', 'Katrina', 'Roy', '11@myport.ac.uk', '0', 'Away');
 -- Lecturers
 INSERT INTO user_account (	
 		user_id,
@@ -54,7 +54,7 @@ INSERT INTO user_account (
 		user_role,
 		user_status)
 VALUES 
-('17', 'Dustin', 'Mcgrath', 'dmcgrath@port.ac.uk', '0', 'Lecturer', 'Away'),
-('18', 'Sid', 'Schaefer', 'sschaefer@port.ac.uk', '0', 'Lecturer', 'Online'),
-('19', 'Khadeejah', 'Holden', 'kholden@port.ac.uk', '0', 'Lecturer', 'Busy');
+('21', 'Dustin', 'Mcgrath', 'dmcgrath@port.ac.uk', '0', 'Lecturer', 'Away'),
+('22', 'Sid', 'Schaefer', 'sschaefer@port.ac.uk', '0', 'Lecturer', 'Online'),
+('23', 'Khadeejah', 'Holden', 'kholden@port.ac.uk', '0', 'Lecturer', 'Busy');
 
