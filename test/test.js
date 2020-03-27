1564544ac82ad94f09d95704a7b0754595ef3cc1
Zarhad-01 <Zahidawan34@gmail.com>
2020-03-27 01:08:53 +0000

    user register test

--- test/test.js
+++ test/test.js
@@ -22,9 +22,50 @@ const request = require('request');
 const fetch = require('node-fetch');
 const server = require('../server');
 
+
+// Insuring QUnit works
 QUnit.test("Making sure testing works, 2 * 4 should be 8", function(assert){
   assert.equal(2*4, 8, "2 * 4 = 8")
 })
+
+//Testcase
+const url = 'http://localhost:8080/';
+const EMAIL = 'up000000@myport.ac.uk';
+const password = '1234';
+const hashPass = '$2a$10$1fB5pnQItRI8LKoPcsMB0O/eddkmnJz6bBRx55ecYeqJXiCSC.qD6';
+
+const testUser = {
+  login:{
+    email: EMAIL,
+    pass: password,
+  },
+  register:{
+    fName: "Jomes",
+    lName: "Smorths",
+    email: EMAIL,
+    pass: password,
+  },
+}
+
+//
+async function httpPost(request, obj){
+  const res = await fetch(url + request, {
+    method: "post",
+    body: JSON.stringify(obj),
+    headers: {
+        'Content-Type': 'application/json'
+    }
+  });
+  return await res.json();
+}
+
+// Testing the application
+QUnit.test("User can register", async function(assert){
+  testRes = await httpPost ("register", testUser.register)
+  assert.equal(testRes.success, True, "User has been able to register successfully")
+})
+
+
 
 
 
