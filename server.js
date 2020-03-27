609b170b7deb76e47e73d9a7207d1c740f86bd2d
Zarhad-01 <Zahidawan34@gmail.com>
2020-03-27 01:52:36 +0000

    Update server.js

--- server.js
+++ server.js
@@ -105,15 +105,15 @@ app.post('/register', checkNotLoggedIn, async (req, res, next) => {
     // TODO: send confirmation email
 
     // registration worked so send to login page
-    res.redirect({state: req.session.state}, '/register/confirm-email');
+    res.redirect( '/register/confirm-email');
 
-    res.json({success: true, reason: "Account was created"})
+    res.json({state: req.session.state},{success: true, reason: "Account was created"})
 
   } catch (err) {
     console.log(err);
     // something went wrong, try again.
-    res.redirect({state: req.session.state},'/login?registration_failed=true' );
-    res.json({success: true, reason: "Account was created"})
+    res.redirect('/login?registration_failed=true' );
+    res.json({state: req.session.state},{success: true, reason: "Account was created"})
   }
 })
 // handles logging out
